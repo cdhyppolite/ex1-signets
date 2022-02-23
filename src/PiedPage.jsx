b367734e5a41a3b461505e97b82be218a449626d
Carl-David Hyppolite <e1975958@cmaisonneuve.qc.ca>
2022-02-23 13:54:10 -0500

    bouton + en bas

--- src/PiedPage.jsx
+++ src/PiedPage.jsx
@@ -3,7 +3,10 @@ import './PiedPage.scss';
 export default function PiedPage() {
     return (
         <footer className="PiedPage">
-            &copy;{new Date().getFullYear()} - TIM Maisonneuve - Tous droits réservés
+            <div className='btnPlus'>
+            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+            </div>
+            <h1>&copy;{new Date().getFullYear()} - TIM Maisonneuve - Tous droits réservés</h1>
         </footer>
     );
 }
