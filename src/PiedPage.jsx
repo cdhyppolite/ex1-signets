4b6d1628c972c5b39f77942e9274341f0a5ce17f
Carl-David Hyppolite <e1975958@cmaisonneuve.qc.ca>
2022-02-24 10:40:33 -0500

    sans mat UI

--- src/PiedPage.jsx
+++ src/PiedPage.jsx
@@ -4,7 +4,7 @@ export default function PiedPage() {
     return (
         <footer className="PiedPage">
             <div className='btnPlus'>
-            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+            <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
             </div>
             <h1>&copy;{new Date().getFullYear()} - TIM Maisonneuve - Tous droits réservés</h1>
         </footer>
--- src/Signet.jsx
+++ src/Signet.jsx
@@ -3,15 +3,15 @@ import './Signet.scss';
 export default function Signet(props) {
         let ficherImage ="img/signets/" + props.sid + ".webp";
     return (
-        <article className="Signet">
+        <article className="Signet" style={{ backgroundColor: props.couleur }}>
         <div className="Signet__divImg">
             <div className='btnSig haut'>
                 <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
             </div>
             <img src={"img/signets/" + props.sid + ".webp"} alt={props.nom} />
         </div> 
-        <div className="titre" style={{ backgroundColor: props.couleur }}>
-            {props.nom}
+        <div className="titre">
+            <h1>{props.nom}</h1>
             <h6>Modifié le: <i>{props.date}</i></h6>
             <div className='btnSig bas'>
                 <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path> </svg>
