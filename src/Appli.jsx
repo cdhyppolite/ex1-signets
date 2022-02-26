c55adce5618da3ddec2af94bf94d759b813a63ee
cdhyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-02-25 19:55:59 -0500

    Update Appli.jsx

--- src/Appli.jsx
+++ src/Appli.jsx
@@ -2,8 +2,6 @@ import './Appli.scss';
 import Entete from './Entete';
 import PiedPage from './PiedPage';
 import ListeSignets from './ListeSignets';
-// import ListeProduits from './ListeProduits';
-// import {useState} from 'react';
 
 export default function Appli() {
   return (
