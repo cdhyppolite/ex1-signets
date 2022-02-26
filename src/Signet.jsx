17d29e83fbbec7e8aa8dc665e3503dd801afbb18
cdhyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-02-25 20:12:32 -0500

    Update Signet.jsx

--- src/Signet.jsx
+++ src/Signet.jsx
@@ -3,7 +3,7 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import NotesIcon from '@mui/icons-material/Notes';
 
 export default function Signet(props) {
-        let ficherImage ="img/signets/" + props.sid + ".webp";
+        //let ficherImage ="img/signets/" + props.sid + ".webp";
     return (
         <article className="Signet" style={{ backgroundColor: props.couleur }}>
         <div className="Signet__divImg">
